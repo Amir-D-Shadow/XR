e686e2cdbc69dffe2e69399412366fa51ca851f3
Amir Lau <67386096+Amir-D-Shadow@users.noreply.github.com>
2021-09-05 18:11:21 +0800

    Add files via upload

--- object_detection_model/alpha/alpha_loss.py
+++ object_detection_model/alpha/alpha_loss.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import tensorflow.keras.backend as K
 import numpy  as np
 from matplotlib import pyplot as plt
+from AttentionModule_lambda_version import AttentionModule
 
 class alpha_loss(tf.keras.losses.Loss):
 
@@ -284,22 +285,38 @@ if __name__ == "__main__":
    y_true = np.clip(y_true,0.0,1.0)
    data = tf.constant(value= np.random.randn(10,40,40,3),dtype=np.float64)
 
+   attention_info = {}
+   attention_info["CBL_1"] = (20,3,1,"same")
+   attention_info["conv_query"] = (20,3,1,"same")
+   attention_info["conv_keys"] = (20,3,1,"same")
+   attention_info["conv_values"] = (20,3,1,"same")
+
    x = tf.keras.layers.Input(shape =(40,40,3))
    strategy = tf.distribute.MirroredStrategy(cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
 
    with strategy.scope():
      
+     """
      k1 = tf.keras.layers.BatchNormalization(axis=-1)(x)
-     h1 = tf.keras.layers.Conv2D(1024,3,1,"valid",data_format="channels_last")(k1)
+     h1 = tf.keras.layers.Conv2D(1024,3,1,"valid",data_format="channels_last",activation=tf.keras.layers.LeakyReLU())(k1)
      h2 = tf.keras.layers.BatchNormalization(axis=-1)(h1)
      h3 = tf.keras.layers.LeakyReLU()(h2)
-     drop_h = tf.keras.layers.SpatialDropout2D(0.5)(h3)
-     h4 = tf.keras.layers.Conv2D(1024,3,1,"same",data_format="channels_last")(drop_h)
+     h4 = tf.keras.layers.Conv2D(1024,3,1,"same",data_format="channels_last")(h3)
      h5 = tf.keras.layers.BatchNormalization(axis=-1)(h4)
      h6 = tf.keras.layers.LeakyReLU()(h5)
      bat1 = tf.keras.layers.BatchNormalization(axis=-1)(h6)
      k2 = tf.keras.layers.Conv2D(89,3,1,"same",data_format="channels_last",activation="sigmoid")(bat1)
 
+     """
+     k1 = tf.keras.layers.Conv2D(512,3,1,"valid",data_format="channels_last",activation=tf.keras.layers.LeakyReLU())(x)
+     h2 = tf.keras.layers.BatchNormalization(axis=-1)(k1)
+     a1 = tf.keras.layers.LeakyReLU()(h2)
+     AttentionModule_1 = AttentionModule(attention_info)(a1)
+     p2 = tf.keras.layers.Conv2D(512,3,1,"same",data_format="channels_last")(AttentionModule_1)
+     h3 = tf.keras.layers.BatchNormalization(axis=-1)(p2)
+     a2 = tf.keras.layers.LeakyReLU()(h3)
+     k2 = tf.keras.layers.Conv2D(89,3,1,"same",data_format="channels_last",activation="sigmoid")(a2)
+     
      model = tf.keras.Model(inputs=x,outputs=k2)
 
      model.compile(optimizer="adam",loss=alpha_loss())
