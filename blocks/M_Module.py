c76e75d28b8e543aff6a644de8f00571e3763916
Amir Lau <67386096+Amir-D-Shadow@users.noreply.github.com>
2021-10-05 19:02:49 +0800

    Add files via upload

--- blocks/M_Module.py
+++ blocks/M_Module.py
@@ -7,9 +7,11 @@ class M_Module(tf.keras.Model):
 
         super(M_Module,self).__init__(**kwargs)
 
-        self.num_C = M_info["num_C"]
+        self.feat_scale = M_info["feat_scale"]
 
-        self.MSL = MagicSquareLayer(self.num_C)
+        self.feat_alpha = M_info["feat_alpha"]
+
+        self.MSL = MagicSquareLayer(feat_alpha = self.feat_alpha,feat_scale = self.feat_scale)
 
         self.BN = tf.keras.layers.BatchNormalization(axis=-1)
 
@@ -25,3 +27,10 @@ class M_Module(tf.keras.Model):
         output_LR = self.LR(BN)
 
         return output_LR
+
+
+    def graph_model(self,dim):
+
+        x = tf.keras.layers.Input(shape=dim)
+        
+        return tf.keras.Model(inputs=x,outputs=self.call(x))
--- blocks/MagicSquareLayer.py
+++ blocks/MagicSquareLayer.py
@@ -4,33 +4,50 @@ import numpy as np
 
 class MagicSquareLayer(tf.keras.layers.Layer):
 
-    def __init__(self,feat_C = 1,**kwargs):
+    def __init__(self,feat_alpha=0.03,feat_scale = 1,**kwargs):
 
         super(MagicSquareLayer,self).__init__(**kwargs)
 
-        self.num_C = feat_C
-
+        self.layer_scale = feat_scale
+        
+        self.layer_alpha = feat_alpha
+        
 
     def build(self,input_shape):
 
-        self.M_kernel = tf.Variable(name="Magic Kernel",initial_value=tf.random_normal_initializer()(shape=(input_shape[-1],input_shape[-1]*self.num_C),dtype=tf.float64),trainable=True)
+        #scale dimension
+        H_W = input_shape[1]*input_shape[2] / self.layer_scale
 
-        self.M_bias = tf.Variable(name="Magic Bias",initial_value=tf.random_normal_initializer()(shape=(1,1,1,input_shape[-1]*self.num_C),dtype=tf.float64),trainable=True)
+        H_W = tf.cast(H_W,dtype=tf.int64)
+
+        #initialization
+        self.K_Kernel = tf.Variable(name="K Kernel",initial_value=tf.random_normal_initializer()(shape=(input_shape[-1],H_W),dtype=tf.float64),trainable=True)
+
+        self.V_Kernel = tf.Variable(name="V Kernel",initial_value=tf.random_normal_initializer()(shape=(H_W,input_shape[-1]),dtype=tf.float64),trainable=True)
+
+        self.V_bias = tf.Variable(name="V Bias",initial_value=tf.random_normal_initializer()(shape=(1,1,1,input_shape[-1]),dtype=tf.float64),trainable=True)
 
 
     def call(self,inputs):
 
         """
         inputs : (m,h,w,c)
-        M_kernel : (c,c)
-        M_bias: (1,1,1,c) 
+        K_Kernel : (c,h x w)
+        V_Kernel : (h x w,c)
+        V_bias: (1,1,1,c) 
         """
 
         #cast input type to fit the kernel's type
         x = tf.keras.backend.cast(inputs,dtype=tf.float64)
 
-        #output_M: (m,h,w,c)
-        output_M = tf.matmul(x,self.M_kernel) + self.M_bias
+        #output_M: (m,H,W,H_W)
+        output_M = tf.matmul(x,self.K_Kernel)
+
+        #output_M: (m,H,W,H_W)
+        output_M = tf.math.maximum(output_M, self.layer_alpha * output_M )
+
+        #output_M: (m,H,W,c)
+        output_M = tf.matmul(output_M,self.V_Kernel) + self.V_bias
 
         return output_M
 
