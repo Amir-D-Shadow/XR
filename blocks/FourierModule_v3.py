bf02a309b9aa1b433a6934b0a44baa2414480009
Amir Lau <67386096+Amir-D-Shadow@users.noreply.github.com>
2021-11-14 18:44:59 +0800

    Add files via upload

--- blocks/FourierModule_v3.py
+++ blocks/FourierModule_v3.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 from tensorflow.python.keras import backend as K
 import numpy as np
 from ResMLP import ResMLP
+import CBL as CBL
 
       
-class FourierModule(tf.keras.layers.Layer):
+class FourierModule(tf.keras.Model):
 
 
-   def __init__(self,units,MLP_num,output_units,**kwargs):
+   def __init__(self,F_info,**kwargs):
 
       """
 
@@ -26,13 +27,14 @@ class FourierModule(tf.keras.layers.Layer):
 
       super(FourierModule,self).__init__(**kwargs)
 
-      self.units = units
-      self.MLP_num = MLP_num
+      self.units = F_info["units"]
+      self.MLP_num = F_info["MLP_num"]
 
 
       self.input_MLP = tf.keras.layers.Dense(self.units)
       self.input_norm = tf.keras.layers.LayerNormalization(axis=-1)
       self.input_act = tf.keras.layers.LeakyReLU()
+      self.input_drop = tf.keras.layers.Dropout(rate=0.5)
 
       self.ResMLP_block = {}
 
@@ -40,18 +42,12 @@ class FourierModule(tf.keras.layers.Layer):
 
          self.ResMLP_block[f"ResMLP_{i}"] = ResMLP(self.units)
 
-      self.output_units = output_units
+      self.output_units = F_info["output_units"]
       
       self.output_MLP = tf.keras.layers.Dense(self.output_units)
       self.output_act = tf.keras.layers.LeakyReLU()
 
-      
-      self.BN_1 = tf.keras.layers.BatchNormalization(axis=-1)
-      self.conv_output = tf.keras.layers.Conv2D(filters=self.units,kernel_size=3,strides=1,padding="same",data_format="channels_last",activation=tf.keras.layers.LeakyReLU())
-
-   def build(self,input_shape):
-
-      self.CBL_1 = CBL(input_shape[-1],1,1,"same")
+      self.CBL_output = CBL(64,1,1,"same")
 
 
    def call(self,inputs,train_flag=True):
@@ -95,8 +91,9 @@ class FourierModule(tf.keras.layers.Layer):
       input_MLP = self.input_MLP(feat_analysis_inputs)
       input_norm = self.input_norm(input_MLP,training=train_flag)
       input_act = self.input_act(input_norm)
+      input_drop = self.input_drop(input_act)
 
-      ResMLP_block = input_act
+      ResMLP_block = input_drop#input_act
       
       for i in range(1,self.MLP_num+1):
 
@@ -109,7 +106,7 @@ class FourierModule(tf.keras.layers.Layer):
       ################# ResMLP Filtering #################
 
       # Tensor output_act [TU1,TL1,TU2,TL2,TU3,TL3,TU4,TL4]
-      final_tensor = inputs
+      final_list = []
 
       for i in range(self.output_units//2):
 
@@ -117,22 +114,27 @@ class FourierModule(tf.keras.layers.Layer):
          TU = output_act[:,0]
          TU = TU[:,tf.newaxis,tf.newaxis,tf.newaxis]
          TU = K.cast(TU,K.dtype(feat_FT_real))
-         TU_filtering = feat_FT_real * K.maximum( TU -feat_FT_real ,0 ) / (TU*0.1 -feat_FT_real + 1)
+         TU_filtering = feat_FT_real * K.maximum( TU -feat_FT_real ,0 ) / (TU -feat_FT_real + 1)
 
          TL = output_act[:,1]
          TL = TL[:,tf.newaxis,tf.newaxis,tf.newaxis]
          TL = K.cast(TL,K.dtype(feat_FT_real))
-         TL_filtering = TU_filtering * K.maximum( TU_filtering - TL, 0 )  / (TU_filtering - TL*0.1 + 1)
+         TL_filtering = TU_filtering * K.maximum( TU_filtering - TL, 0 )  / (TU_filtering - TL + 1)
          
-         feat_intv1 = self.CBL_1(TL_filtering,train_flag)
-            
+         TL_filtering = tf.complex(TL_filtering,feat_FT_imag)
+         TL_filtering = K.cast(TL_filtering,K.dtype(feat_FT))
 
-         final_tensor = final_tensor + feat_intv1
+         feat_intv1 = tf.signal.ifft3d( TL_filtering )
+         feat_intv1 = tf.math.real( feat_intv1 )
+         feat_intv1 = K.cast(feat_intv1,K.dtype(inputs))
+
+         final_list.append(feat_intv1)
+
 
 
       #final result
-      final_tensor = self.BN_1(final_tensor)
-      final_tensor = self.conv_output(final_tensor)
+      final_tensor = tf.keras.layers.concatenate(final_list,axis=-1)
+      final_tensor = self.CBL_output(final_tensor,train_flag)
 
       final_tensor = K.cast(final_tensor,K.dtype(inputs))
 
@@ -144,6 +146,5 @@ class FourierModule(tf.keras.layers.Layer):
       x = tf.keras.layers.Input(shape=dim)
       
       return tf.keras.Model(inputs=x,outputs=self.call(x))
-
 
       
